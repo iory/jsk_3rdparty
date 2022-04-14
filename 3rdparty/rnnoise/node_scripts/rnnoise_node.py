95502e9456c748b52a9ccd2aba55dde488211900
iory <ab.ioryz@gmail.com>
2022-04-14 11:47:05 +0900

    Add

--- 3rdparty/rnnoise/node_scripts/rnnoise_node.py
+++ 3rdparty/rnnoise/node_scripts/rnnoise_node.py
@@ -10,29 +10,41 @@ class RNNoiseNode(object):
 
     def __init__(self):
         self.denoiser = RNNoise()
+        self.denoiser.channels = 1
+        # self.denoiser.sample_rate = 16000
+        self.audio_buffer = b''
+        self.pub = rospy.Publisher(
+            '~output', AudioData,
+            queue_size=1)
         self.subscribe()
 
     def subscribe(self):
         self.sub_audio = rospy.Subscriber(
             '~audio', AudioData,
             callback=self.callback,
-            queue_size=1)
+            queue_size=100)
 
     def callback(self, audio_msg):
-        # self.dtype = 'int16'
-        # audio_buffer = np.frombuffer(audio_msg.data, dtype=self.dtype)
-        # audio_buffer = audio_buffer[0::self.n_channel]
-        # print(len(audio_buffer), len(audio_msg.data))
-        pass
-        # voice_prob_threshold = 0.8
-        # for i in range(buffer_size_ms, len(audio), buffer_size_ms):
-        #     denoised_audio += self.denoiser.filter(audio[i-buffer_size_ms:i].raw_data, sample_rate=audio.frame_rate,
-        #                                       voice_prob_threshold=voice_prob_threshold)
-        # if len(audio) % buffer_size_ms != 0:
-        #     denoised_audio += self.denoiser.filter(audio[len(audio)-(len(audio)%buffer_size_ms):].raw_data, sample_rate=audio.frame_rate,
-        #                                       voice_prob_threshold=voice_prob_threshold)
-
-        # self.denoiser.write_wav('test_denoised_stream.wav', denoised_audio, sample_rate=audio.frame_rate)
+        n_channel = self.denoiser.channels
+        sample_rate = 16000
+        buffer_size_ms = 10
+        sample_width = 16 // 8
+        buffer_size_s = buffer_size_ms * 0.001
+        n_frame = (len(audio_msg.data) // sample_width) // n_channel
+        required_frame_count = int(buffer_size_s * sample_rate)
+        self.audio_buffer += audio_msg.data
+        voice_prob_threshold = 0.8
+        step = required_frame_count * n_channel * sample_width
+        i = 0
+        # print(len(self.audio_buffer))
+        for i in range(step, len(self.audio_buffer), step):
+            denoised_audio = self.denoiser.filter(
+                self.audio_buffer[i - step:i],
+                sample_rate=sample_rate,
+                voice_prob_threshold=voice_prob_threshold)
+            print(len(denoised_audio))
+            self.pub.publish(AudioData(data=denoised_audio))
+        self.audio_buffer = self.audio_buffer[i:]
 
 
 if __name__ == '__main__':
